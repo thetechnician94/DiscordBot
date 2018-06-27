68097ac7cb6fd1c7a8903680d95a5259ad5787d9
Tyler <94tylercraig@gmail.com>
2018-06-27 17:18:02 -0400

    Main

--- Bot/bot.js
+++ Bot/bot.js
@@ -24,25 +24,20 @@ bot.on('ready', function (evt) {
 
 //look at text commands
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '$') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
+            case "test": sendMsg("test reply");           
+        }
      }
 });
+
+void sendMsg(msg){
+	bot.sendMessage({to: channelID,message: msg});	
+}
+	
 
 
 https://discordapp.com/oauth2/authorize?&client_id=461635519189614593&scope=bot&permissions=0
