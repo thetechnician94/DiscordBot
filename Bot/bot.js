9b6362d1b84746ac7421162f5a81b65b232f19e3
Tyler <94tylercraig@gmail.com>
2018-06-27 17:56:56 -0400

    Main

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,26 +1,13 @@
 var Discord = require('discord.io');
-var logger = require('winston');
 var auth = require('./auth.json');
-
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+
+bot.on('ready', function (evt) {});
 
 //look at text commands
 bot.on('message', function (user, userID, channelID, message, evt) {
