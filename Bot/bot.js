dfaab885f50790645fcafdc92c478181dafe4e63
Tyler <94tylercraig@gmail.com>
2018-06-27 19:58:32 -0400

    Authetication

--- Bot/bot.js
+++ Bot/bot.js
@@ -11,10 +11,50 @@ bot.on('ready', () => {
 
 //look at text commands
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  var args = msg.content.substring(1).split(' ');
+  var cmd = args[0];
+  args = args.splice(1);
+  if (msg.content.substring(0,1)=="$") {
+   switch(cmd) {
+            case "test": msg.reply("test reply");   
+				break;
+			case "version": msg.reply("1.0"); 
+				break;
+			case "whoami": 
+				msg.reply(msg.author.username);
+				break;
+			case "cleanMsgs":
+				msg.reply(bot.sweepMessages(10));
+				break;
+			case "botinfo":
+				if(authenticate("Admin",msg)){
+					msg.reply("\nClient: "+bot.user+"\nPing: "+bot.ping+"\nStatus: "+bot.status+"\nUptime: "+(bot.uptime/1000) +" (secs)"+"\nReady at: "+bot.readyAt);
+				}
+				break;
+			default: msg.reply("Unrecognized Command");   
+     }
+  }
+  else{
+	return;
   }
 });
 
 
 bot.login(auth.token);
+
+
+function authenticate(roleName,msg){
+	if(!msg.guild.available){
+		msg.reply("Guild not available");
+		return;
+	}
+	let role = msg.guild.roles.find("name", roleName);
+	if(role===null){
+		msg.reply("Role \""+roleName+"\" not found");
+		return;
+	}
+	if(msg.member.roles.has(role.id)) {
+		return true;
+	}
+	return false;
+}
