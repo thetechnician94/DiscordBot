0d335c437b759dc447ebf8f143d690c8ba4ae37c
Tyler <94tylercraig@gmail.com>
2018-06-27 19:22:07 -0400

    Main

--- Bot/bot.js
+++ Bot/bot.js
@@ -10,32 +10,11 @@ bot.on('ready', () => {
 
 
 //look at text commands
-bot.on('message', function (user, userID, channelID, message, evt) {
-	
-	if (message.substring(0, 1) == '$') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        args = args.splice(1);
-        switch(cmd) {
-            case "test": sendMsg(channelID,message,"test reply");   
-				break;
-			case "version": sendMsg(channelID,message,"1.0"); 
-				break;
-			case "disconnect": 
-				sendMsg(channelID,message,"Bye");
-				bot.disconnect();
-				break;
-			case "d": 
-				sendMsg(channelID,message,bot.getUser(userID));
-				break;
-//			case "deleteMessages": DCP.deleteMessages();
-//				break;
-			default: sendMsg(channelID,message,"Unrecognized Command");   
-        }
-     }
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 
-function sendMsg(channelID,message,msg){
-	bot.sendMessage({to: channelID,message: msg});	
-}
+client.login('token');
