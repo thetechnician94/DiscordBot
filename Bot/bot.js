d7182ebd498be630c65df6272df14b436d880297
Tyler <94tylercraig@gmail.com>
2018-06-27 22:02:36 -0400

    Main

--- Bot/bot.js
+++ Bot/bot.js
@@ -6,17 +6,22 @@ var bot = new Discord.Client();
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
-
+var commandSymbol="%";
 //look at text commands
 bot.on('message', msg => {
   var args = msg.content.substring(1).split(' ');
   var cmd = args[0];
   args = args.splice(1);
-  if (msg.content.substring(0,1)=="$") {
+  if (msg.content.substring(0,1)==commandSymbol) {
    switch(cmd) {
 			case "cleanMsgs":
-				cleanMsgs(msg,args);
-				break;		
+				if(authenticate("Admin",msg)){
+					cleanMsgs(msg,args);
+				}
+				break;
+			case "searchMsgs":
+				searchMsgs(msg,args);
+				break;
 			case "botinfo":
 				if(authenticate("Admin",msg)){
 					msg.reply("\nVersion: 1.0\nClient: "+bot.user+"\nPing: "+bot.ping+"\nStatus: "+bot.status+"\nUptime: "+(bot.uptime/1000) +" (secs)"+"\nReady at: "+bot.readyAt);
@@ -47,32 +52,50 @@ function authenticate(roleName,msg){
 	return false;
 }
 
+function searchMsgs(msg,args){
+	if(args.length==0){
+				return;
+	}
+	msg.channel.fetchMessages().then(function(data){
+			var msgs = data.array();
+			for(var i=0;i<msgs.length;i++){
+				if(msgs[i].content.substring(0,1)==commandSymbol || msgs[i].author.bot){
+					continue;
+				}
+				for(var k=0;k<args.length;k++){
+					if (msgs[i].content.includes(args[k])){
+						msg.reply("\n"+msgs[i].author.username+"\n\n"+msgs[i].content+"\n\n"+msgs[i].createdAt);
+						break;;
+					}
+				}
+			}			
+	});
+}
+
 function cleanMsgs(msg,args){
-	if(authenticate("Admin",msg)){
-		msg.channel.fetchMessages().then(function(data){
-				if(args.length>0){
-					if(args[0]==="all"){
-						msg.channel.bulkDelete(data);	
-					}
-					else{
-						var msgs = data.array();
-						var num=parseInt(args[0]);
-						for(var i=0;i<msgs.length && i<=num;i++){
-							if(!msgs[i].pinned){
-								msgs[i].delete();
-							}
-						}
-					}
+	msg.channel.fetchMessages().then(function(data){
+			if(args.length>0){
+				if(args[0]==="all"){
+					msg.channel.bulkDelete(data);	
 				}
 				else{
 					var msgs = data.array();
-					for(var i=0;i<msgs.length;i++){
+					var num=parseInt(args[0]);
+					for(var i=0;i<msgs.length && i<=num;i++){
 						if(!msgs[i].pinned){
 							msgs[i].delete();
 						}
 					}
 				}
-				
-		});
-	}
+			}
+			else{
+				var msgs = data.array();
+				for(var i=0;i<msgs.length;i++){
+					if(!msgs[i].pinned){
+						msgs[i].delete();
+					}
+				}
+			}
+			
+	});
 }
