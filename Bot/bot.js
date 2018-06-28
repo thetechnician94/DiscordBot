3b15b03acf0022b679b284b8cfaca94bd1a936ca
Tyler <94tylercraig@gmail.com>
2018-06-28 17:56:20 -0400

    Main

--- Bot/bot.js
+++ Bot/bot.js
@@ -69,7 +69,10 @@ function updateVoiceChannels(oldUser,newUser){
 		var members = newUser.voiceChannel.members.array();
 		if(!newUser.user.bot && (oldUser.voiceChannel==null || oldUser.voiceChannel != newUser.voiceChannel)){
 			newUser.voiceChannel.join().then(connection=>{
-				const dispatcher = connection.playFile("/home/ad/Bot/connected.mp3",{bitrate:"auto",passes:3});		
+				const dispatcher = connection.playFile("/home/ad/Bot/connected.mp3",{bitrate:"auto",passes:3});
+				dispatcher.on("end", end => {
+					connection.disconnect();
+				}); 		
 			}).catch(console.error);;
 		}
 		var members = newUser.voiceChannel.members.array();
@@ -93,11 +96,14 @@ function updateVoiceChannels(oldUser,newUser){
 			return;
 		}	
 		var members = oldUser.voiceChannel.members.array();
-		if(!oldUser.user.bot && (newUser.voiceChannel==null || oldUser.voiceChannel != newUser.voiceChannel)){
+		/*if(!oldUser.user.bot && (newUser.voiceChannel==null || oldUser.voiceChannel != newUser.voiceChannel)){
 			oldUser.voiceChannel.join().then(connection=>{
 				const dispatcher = connection.playFile("/home/ad/Bot/disconnected.mp3",{bitrate:"auto",passes:3});
+				dispatcher.on("end", end => {
+					connection.disconnect();
+				}); 		
 			}).catch(console.error);;
-		}
+		}*/
 		if(members.length==0){
 			oldUser.voiceChannel.setName("Open Voice Channel");
 			return;
